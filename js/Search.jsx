3b310e17ef7babded9cde4dfcad7e72be1724eb9
motto813 <hammerschmidtpaul@gmail.com>
2017-10-05 16:42:54 -0500

    Show card list uses imdbID as key

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => <div className="search">{preload.shows.map(show => <ShowCard {...show} />)}</div>;
+const Search = () => (
+  <div className="search">{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+);
 
 export default Search;
