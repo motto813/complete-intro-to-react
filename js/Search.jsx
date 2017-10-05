fbbe542880f35af1bcb514eed8092025a584954d
motto813 <hammerschmidtpaul@gmail.com>
2017-10-05 16:39:34 -0500

    spread operator creates ShowCards with top level attributes, prop types define required show card attributes

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => <div className="search">{preload.shows.map(show => <ShowCard show={show} />)}</div>;
+const Search = () => <div className="search">{preload.shows.map(show => <ShowCard {...show} />)}</div>;
 
 export default Search;
