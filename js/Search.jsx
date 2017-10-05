4362d5186177b3c739bf52d0b03bbadaff216adf
motto813 <hammerschmidtpaul@gmail.com>
2017-10-05 17:15:43 -0500

    wrapper for show cards and poster image styled with styled components. div wrapper for show cards in list

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,7 +3,9 @@ import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
 const Search = () => (
-  <div className="search">{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+  <div className="search">
+    <div>{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}</div>
+  </div>
 );
 
 export default Search;
