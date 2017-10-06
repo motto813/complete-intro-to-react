b8b6b7f9da00d7b3b3b2bdbcbd42733f68fd2e37
motto813 <hammerschmidtpaul@gmail.com>
2017-10-06 13:29:24 -0500

    test for number of shows rendered from given search term

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -14,4 +14,15 @@ test("Search should render correct amount of shows", () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
-xtest("");
+test("Search should render correct amount of shows based on search term", () => {
+  const searchWord = "black";
+  const component = shallow(<Search />);
+  component.find("input").simulate("change", { target: { value: searchWord } });
+
+  // instead of copying code from Search component as seen below:
+  // better idea: create a module, test it's behavior, then use that module here
+  const showCount = preload.shows.filter(
+    show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
