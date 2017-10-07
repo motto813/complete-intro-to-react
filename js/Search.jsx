0dc45f9b2bc1bf7baa1107929100df5767735691
motto813 <hammerschmidtpaul@gmail.com>
2017-10-06 19:02:01 -0500

    Show flow type. Search class sets prop type of shows as Array of Shows. Search route sends preload shows thru props

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,11 +2,14 @@
 
 import React, { Component } from "react";
 import ShowCard from "./ShowCard";
-import preload from "../data.json";
 
 class Search extends Component {
   state = {
     searchTerm: ""
+  };
+
+  props: {
+    shows: Array<Show>
   };
 
   handleSearchTermChange = (
@@ -28,7 +31,7 @@ class Search extends Component {
           />
         </header>
         <div>
-          {preload.shows
+          {this.props.shows
             .filter(
               show =>
                 `${show.title} ${show.description}`
