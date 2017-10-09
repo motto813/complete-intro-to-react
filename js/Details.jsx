ac02dcb5c9f5c070426353e0244054beae16cc17
motto813 <hammerschmidtpaul@gmail.com>
2017-10-09 12:05:07 -0500

    details make ajax call to get rating. spinner component for showing loading before response

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,32 +1,54 @@
 // @flow
 
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import Header from "./Header";
+import Spinner from "./Spinner";
 
-const Details = (props: { show: Show }) => {
-  // es6 destructuring below:
-  // equivalent to const title = props.show.title; etc.
-  const { title, description, year, poster, trailer } = props.show;
-  return (
-    <div className="details">
-      <Header />
-      <section>
-        <h1>{title}</h1>
-        <h2>({year})</h2>
-        <img src={`/public/img/posters/${poster}`} alt={`${title}`} />
-        <p>{description}</p>
-      </section>
-      <div>
-        <iframe
-          src={`https://wwww.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
-          frameBorder="0"
-          allowFullScreen
-          title={`Trailer for ${title}`}
-        />
+class Details extends Component {
+  state = {
+    apiData: { rating: "" }
+  };
+  componentDidMount() {
+    axios.get(`http://localhost:3000/${this.props.show.imdbID}`).then((response: { data: { rating: string } }) => {
+      this.setState({ apiData: response.data });
+    });
+  }
+  props: {
+    show: Show
+  };
+  render() {
+    // es6 destructuring below:
+    // equivalent to const title = props.show.title; etc.
+    const { title, description, year, poster, trailer } = this.props.show;
+    let ratingComponent;
+    if (this.state.apiData.rating) {
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+    } else {
+      ratingComponent = <Spinner />;
+    }
+    return (
+      <div className="details">
+        <Header />
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          {ratingComponent}
+          <img src={`/public/img/posters/${poster}`} alt={`${title}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://wwww.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder="0"
+            allowFullScreen
+            title={`Trailer for ${title}`}
+          />
+        </div>
+        {/* <pre><code>{JSON.stringify(props, null, 4)}</code></pre> */}
       </div>
-      {/* <pre><code>{JSON.stringify(props, null, 4)}</code></pre> */}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Details;
